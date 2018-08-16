7c7022403cd2492dc01d7dc3c15285ca30838f72
abshinde221 <ams9mb@virginia.edu>
2018-08-16 15:31:47 -0400

    flask

--- app-backend/server.py
+++ app-backend/server.py
@@ -1,7 +1,18 @@
 from flask import Flask
+import urllib
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	print(urllib.request("http://www.mocky.io/v2/5b75c6932e00006200536216"))
+
+@app.route("/test")
+def test():
+	return urllib.request("http://www.mocky.io/v2/5b75c6932e00006200536216")
+
+
+
+
+if __name__ == "__main__":
+	app.run(debug=True) # run this app
