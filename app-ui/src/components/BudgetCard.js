7ea94acc07f5ad4d1ef20966900e14486fab26e1
aditisundar <aditi.sundar2000@gmail.com>
2018-08-16 21:39:29 -0400

    added graph popup

--- app-ui/src/components/BudgetCard.js
+++ app-ui/src/components/BudgetCard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './BudgetCard.css';
+import Compare from './Compare';
 import { TextField, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+//import PieChart from './Pie';
 
 
 
@@ -12,8 +14,8 @@ class BudgetCard extends Component {
                 <div className={this.props.category}>
                     <h1>{this.props.name}</h1>
                     <h2>${this.props.amount}</h2>
-                    <Edit updateCards={this.props.updateCards} name={this.props.name} />
-                    <Button >What others are doing</Button>
+                    <EditDialog updateCards={this.props.updateCards} name={this.props.name} />
+                    <CompareDialog name={this.props.name} />
 
                 </div>
             </div>
@@ -21,7 +23,7 @@ class BudgetCard extends Component {
     }
 }
 
-class Edit extends React.Component {
+class EditDialog extends React.Component {
     state = {
         value: this.props.value,
         open: false,
@@ -74,6 +76,49 @@ class Edit extends React.Component {
             </Button>
                         <Button onClick={this.handleClose} color="primary">
                             Subscribe
+            </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+class CompareDialog extends React.Component {
+    state = {
+        open: false,
+    };
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.handleClickOpen}>What are others doing?</Button>
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    aria-labelledby="form-dialog-title"
+                >
+                    <DialogTitle id="form-dialog-title">What are others spending on {this.props.name}?</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            See how people like you are spending.
+                        </DialogContentText>
+                        {/*** GRAPH GOES HERE ***
+                            <PieChart />
+                        *** GRAPH GOES HERE ***/}
+
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Close
             </Button>
                     </DialogActions>
                 </Dialog>
